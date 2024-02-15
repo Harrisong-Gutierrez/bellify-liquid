d4824c8ceb1b5e2076dcde6bff7d72bc736f03e1
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-02-15 15:56:32 -0600

    working on footer list

--- src/components/FooterList/index.js
+++ src/components/FooterList/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { list } from "../../../constants/footerList";
+import { v4 as uuidv4 } from "uuid";
 
 const FooterList = () => {
   const renderedList = () => {
     return list.map((section) => (
-      <div>
+      <div key={uuidv4()} className=".bg-primary">
         <h2>{section.title}</h2>
         {section.items.map((item) => (
           <p>{item}</p>
