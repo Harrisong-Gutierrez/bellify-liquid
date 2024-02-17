15a94e9b4c5b8dca43795193c5b180ba83a1eb22
Harrisong <harrygutie7@gmail.com>
2024-02-17 17:31:52 -0600

    working home

--- src/components/Footer/components/FooterList/index.js
+++ src/components/Footer/components/FooterList/index.js
@@ -1,11 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
 import { list } from "../../../../../constants/footerList";
+import Image from "next/image";
 
 const FooterList = () => {
   const renderedList = () => {
     return list.map((section) => (
-      <div className="dropdown" key={uuidv4()}>
-        <h2 className="dropbtn">{section.title}</h2>
+      <div className="dropdown FooterList" key={uuidv4()}>
+       <div className="FooterList-header">
+       <h2 className="FooterList-title dropbtn">{section.title}</h2>
+       <figure>
+        <Image src="/Polígono.png" height={4} width={9} />
+       </figure>
+       </div>
         <div className="dropdown-content">
           {section.items.map((item) => (
             <p key={uuidv4()}>{item}</p>
